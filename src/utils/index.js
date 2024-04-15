ec4c540f853d7cfa7344563db1fc500f9c0ea21e
hamzahmaulana212@gmail.com <hamzahmaulana212@gmail.com>
2024-04-15 19:32:00 +0700

    validation songs

---
+++ src/utils/index.js
@@ -0,0 +1,21 @@
+const mapDBSongs = ({
+  id,
+  title,
+  year,
+  performer,
+  genre,
+  duration,
+  album_id,
+}) => ({
+  id,
+  title,
+  year,
+  performer,
+  genre,
+  duration,
+  albumId: album_id,
+});
+
+module.exports = {
+  mapDBSongs,
+};
