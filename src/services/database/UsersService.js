cdacbc512fd2225b28e8a84f7adc868c40ffcc5a
hamzahmaulana212@gmail.com <hamzahmaulana212@gmail.com>
2024-04-16 19:44:00 +0700

    add service verify user Credentials for login users

--- src/services/database/UsersService.js
+++ src/services/database/UsersService.js
@@ -2,6 +2,7 @@ const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
+const AuthenticationsError = require('../../exceptions/AuthenticationError');
 
 class UsersService {
   constructor() {
@@ -35,6 +36,27 @@ class UsersService {
     if (result.rowCount) {
       throw new InvariantError('Username sudah digunakan');
     }
+  }
+
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rowCount) {
+      throw new AuthenticationsError('Kredensial yang anda berikan salah');
+    }
+
+    const { id, password: hashedPassword } = result.rows[0];
+    const match = await bcrypt.compare(password, hashedPassword);
+
+    if (!match) {
+      throw new AuthenticationsError('Kredensial yang Anda berikan salah');
+    }
+
+    return id;
   }
 }
 
