43c3a82e493d489dede42b46eb8f380547ae3e29
hamzahmaulana212@gmail.com <hamzahmaulana212@gmail.com>
2024-04-18 10:49:43 +0700

    add validator and service playlists

---
+++ src/services/database/PlaylistsService.js
@@ -0,0 +1,24 @@
+const { nanoid } = require('nanoid');
+const { Pool } = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
+
+class PlaylistsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addPlaylist(name, owner) {
+    const id = `playlist-${nanoid(16)}`;
+    const query = {
+      text: 'INSERT INTO playlists VALUES($1, $2, $3) RETURNING id',
+      values: [id, name, owner],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rowCount) {
+      throw new InvariantError('Gagal Menambahkan Playlist');
+    }
+  }
+}
+
+module.exports = PlaylistsService;
