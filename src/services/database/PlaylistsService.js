de0b7ea04db67b10005354e4f28a0868524cda32
hamzahmaulana212@gmail.com <hamzahmaulana212@gmail.com>
2024-04-23 04:01:03 +0700

    add get all playlist for user collaborator

--- src/services/database/PlaylistsService.js
+++ src/services/database/PlaylistsService.js
@@ -30,7 +30,8 @@ class PlaylistsService {
       text: `SELECT playlists.id, playlists.name, users.username
       FROM playlists
       LEFT JOIN users ON playlists.owner = users.id
-      WHERE owner = $1`,
+      LEFT JOIN collaborations ON playlists.id = collaborations.playlist_id
+      WHERE playlists.owner = $1 OR collaborations.user_id = $1`,
       values: [owner],
     };
 
