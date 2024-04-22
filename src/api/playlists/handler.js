184e89c88824c9f724b37f0f0bba36843ebbc713
hamzahmaulana212@gmail.com <hamzahmaulana212@gmail.com>
2024-04-23 04:37:58 +0700

    add delete song in playlist for user collaborator

--- src/api/playlists/handler.js
+++ src/api/playlists/handler.js
@@ -87,7 +87,7 @@ class PLaylistsHandler {
     const { songId } = request.payload;
     const { id: credentialId } = request.auth.credentials;
 
-    await this._service.verifyOwnerPlaylist(playlistId, credentialId);
+    await this._service.verifyPlaylistAccess(playlistId, credentialId);
     await this._service.deleteSongInPlaylist(songId);
 
     return {
